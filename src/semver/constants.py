10b78ef0ce9b566fd36068c6f5dc7146291ac80a
Jonathan Liu <jonathanhliu21@gmail.com>
2022-04-14 13:07:24 -0700

    add comments to each exception message

--- src/semver/constants.py
+++ src/semver/constants.py
@@ -9,30 +9,136 @@ import enum
 RE_PRE = (
     r"^(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)"
     r"(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*$"
-)
-
-RE_BUILD = r"^[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*$"
+)  # for parsing pre-releases
+RE_BUILD = r"^[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*$"  # for parsing build metadata labels
 RE_FULL = (
     r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*"
     r"|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-]"
     r"[0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
-)
+)  # for parsing a full version string
 
 # -------------------- EXCEPTION MSGS --------------------
 
+"""
+When a Version object is added or subtracted (+, - operators) by \
+an unknown type.
+
+* In the + operator of Version, used when rhs is not a str or a VPos enum
+* In the - operator of Version, used when rhs is not a VPos or VRm enum
+"""
 EXC_BAD_TYPE = "Unsupported rhs type for {}: '{}'"
+
+
+"""
+When trying to append a pre-release or build label to a version object \
+that already has a pre-release or build label.
+
+* In the + operator of Version, used when rhs is a str starting with a '+' and \
+the has_build property is True, or when rhs is a str starting with a '-' and \
+the has_pre property is True
+"""
 EXC_CANNOT_ADD = "Cannot add {} that already exists."
+
+
+"""
+When trying to decrement a VersionNumber object or the digit of a Pre object \
+that is currently 0.
+
+* In dec() of VersionNumber, used when _number attribute is 0
+* In dec() of Pre, used when _digit attribute is 0
+"""
 EXC_CANNOT_DEC = "Cannot decrement number to a negative value."
+
+
+"""
+When trying to remove a pre-release or build label of a version object \
+when the object currently does not have a pre-release or build label.
+
+* In the - operator of Version, used when rhs is VRm.PRE and the has_pre \
+property is False, or when rhs is VRm.BUILD and the has_build property is True
+"""
 EXC_CANNOT_RM = "Cannot remove {} that does not exist."
+
+
+"""
+Used when incrementing or decrementing an invalid position of a Version object.
+
+* In inc() of Version, used when the pos argument is not a VPos enum or not \
+part of {VPos.MAJOR, VPos.MINOR, VPos.PATCH, VPos.PRE}
+* In dec() of Version, used when the pos argument is not a VPos enum or not \
+part of {VPos.MAJOR, VPos.MINOR, VPos.PATCH, VPos.PRE}
+"""
 EXC_INVALID_POS = "Unrecognized version position"
+
+
+"""
+Used when there is an error when parsing the string.
+
+* In the constructor of Pre and the string setter, used when the given pre-release \
+string doesn't match the pre-release regex
+* In the constructor of Build and the string setter, used when the build string \
+doesn't match the build regex
+* In parse_version(), used when the version string doesn't match the semantic \
+versioning regex
+"""
 EXC_INVALID_STR = "Invalid {} string."
+
+
+"""
+Used when a Version object is added to an invalid string.
+
+* In the + operator of Version, used when rhs is a string and the first character \
+is not '+' or '-'
+"""
 EXC_INVALID_STR_2 = "Cannot {} an invalid string."
+
+
+"""
+Used when the user is trying to set a VersionNumber object to a negative number.
+
+* In the VersionNumber constructor, used when the number argument is negative
+* In the number setter of VersionNumber, used when the number argument is negative
+"""
 EXC_MUST_POSITIVE = "Number must be positive."
+
+
+"""
+Used in VersionNumber when trying to set a VersionNumber to anything outside of an int.
+
+* In the VersionNumber constructor, used when the number argument not an int
+* In the number setter of VersionNumber, used when the number argument is not an int
+"""
 EXC_MUST_TYPE = "Value must be a(n) {}."
+
+
+"""
+Used in <, <=, >, >=, ==, != operators when comparing with another type.
+
+* In the <, == operators of VersionNumber, used when type(other) != VersionNumber
+* In the <, == operators of Pre, used when type(other) != Pre
+* In the <, == operators of Version, used when type(other) != Version
+"""
 EXC_MUST_CMP = "Must be compared with another {} object."
+
+
+"""
+Used in Pre when no digit exists to increment or decrement
+
+* In inc() of Pre, used when _digit attribute is None
+* In dec() of Pre, used when _digit attribute is None
+"""
 EXC_PRE_NO_VALUE = "No digit to increment/decrement. Make sure the last dot-separated \
                     identifier is a numeric value."
+
+
+"""
+Used in Version when trying to decrement a pre-release that doesn't exist.
+
+* In inc() of Pre, used when pos is VPos.PRE and _pre attribute is None
+* In dec() of Pre, used when pos is VPos.PRE and _pre attribute is None
+"""
 EXC_PRE_NO_VALUE_2 = "Object has no pre-release label."
+
 
 # -------------------- ENUMS --------------------
 
