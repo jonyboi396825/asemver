ee1f6a4bf4ce466ef4133118572db2590c06905a
Jonathan Liu <jonathanhliu21@gmail.com>
2022-04-15 20:37:43 -0700

    add imports to functions

--- src/semver/__init__.py
+++ src/semver/__init__.py
@@ -5,6 +5,13 @@ from .exc import InvalidPositionException as InvalidPositionException
 from .exc import NegativeValueException as NegativeValueException
 from .exc import NoValueException as NoValueException
 from .exc import ParseException as ParseException
+from .operations import add as add
+from .operations import clean as clean
+from .operations import clean_and_parse as clean_and_parse
+from .operations import compare as compare
+from .operations import sub as sub
+from .operations import update as update
+from .operations import valid as valid
 from .version import Version as Version
 from .version import parse_version as parse_version
 
